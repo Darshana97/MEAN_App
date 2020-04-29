c585fb4f9f8115b3b7659955250c174a642f6dc1
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-04-29 09:51:05 +0530

    add  data

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ var port = process.env.PORT || 8000;
 var morgan = require("morgan");
 var mongoose = require("mongoose");
 var User = require("./app/models/user");
-// var bodyParser = require('bod')
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(morgan("dev"));
 
@@ -31,6 +34,7 @@ app.post("/users", function (req, res) {
   user.password = req.body.password;
   user.email = req.body.email;
   user.save();
+  res.send("user created");
 });
 
 app.listen(port, function () {
