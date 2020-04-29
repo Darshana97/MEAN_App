56ad47f191ff8e6dd0dcee294e2635c881155251
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-04-29 10:08:31 +0530

    validation

--- server.js
+++ server.js
@@ -33,8 +33,13 @@ app.post("/users", function (req, res) {
   user.username = req.body.username;
   user.password = req.body.password;
   user.email = req.body.email;
-  user.save();
-  res.send("user created");
+  user.save(function (err) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send("user created");
+    }
+  });
 });
 
 app.listen(port, function () {
