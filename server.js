5e94e19ce364d68e0de9efe74face6fd2f875d85
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-04-29 13:33:31 +0530

    add index page

--- server.js
+++ server.js
@@ -2,16 +2,19 @@ var express = require("express");
 
 var app = express();
 
-var port = process.env.PORT || 8000;
+var port = process.env.PORT || 8080;
 var morgan = require("morgan");
 var mongoose = require("mongoose");
-var User = require("./app/models/user");
 var bodyParser = require("body-parser");
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+var router = express.Router();
+var appRoutes = require("./app/routes/api")(router);
+var path = require("path");
 
 app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+app.use("/api", appRoutes);
 
 mongoose.connect(
   "mongodb://localhost:27017/MeanApp",
@@ -28,29 +31,8 @@ mongoose.connect(
   }
 );
 
-app.post("/users", function (req, res) {
-  var user = new User();
-  user.username = req.body.username;
-  user.password = req.body.password;
-  user.email = req.body.email;
-  if (
-    req.body.username == null ||
-    req.body.username == "" ||
-    req.body.password == null ||
-    req.body.password == "" ||
-    req.body.email == null ||
-    req.body.email == ""
-  ) {
-    res.send("Ensure username,email, and password were provided");
-  } else {
-    user.save(function (err) {
-      if (err) {
-        res.send("Email or Password already taken");
-      } else {
-        res.send("user created");
-      }
-    });
-  }
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/public/app/views/index.html"));
 });
 
 app.listen(port, function () {
