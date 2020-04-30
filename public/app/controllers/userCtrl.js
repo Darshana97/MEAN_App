669d0e9f5d55100793681b6a18fc3eebffb1edbd
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-04-30 11:49:35 +0530

    add loading

--- public/app/controllers/userCtrl.js
+++ public/app/controllers/userCtrl.js
@@ -5,14 +5,16 @@ angular
     var app = this;
 
     this.regUser = function(regData) {
+        app.loading = true;
         app.errorMsg = false;
         console.log("form submitted");
 
         $http.post("/api/users", regData).then(function(data) {
-
             if (data.data.success) {
+                app.loading = false;
                 app.successMsg = data.data.message;
             } else {
+                app.loading = false;
                 app.errorMsg = data.data.message;
             }
         });
